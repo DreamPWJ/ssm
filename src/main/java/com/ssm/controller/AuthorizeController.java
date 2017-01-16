1e30e249f1f613a778ad55e41b7f28d7d3f1b039
406798106@qq.com <406798106@qq.com>
2017-01-16 11:27:03 +0800

    SSM服务端基础框架

--- src/main/java/com/ssm/controller/AuthorizeController.java
+++ src/main/java/com/ssm/controller/AuthorizeController.java
@@ -1,15 +1,13 @@
 package com.ssm.controller;
 
-import com.sun.org.apache.xml.internal.utils.Constants;
 import org.apache.oltu.oauth2.as.request.OAuthAuthzRequest;
 import org.apache.oltu.oauth2.as.response.OAuthASResponse;
 import org.apache.oltu.oauth2.common.error.OAuthError;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.OAuthResponse;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  * Created by panweiji on 2017/1/12 0012.
  * OAuth2授权控制权AuthorizeController
  */
-@Controller
+@RestController
 public class AuthorizeController {
     /**
      * 请求授权方法
@@ -27,9 +25,7 @@ public class AuthorizeController {
      * @return 授权auth code
      */
     @RequestMapping("/authorize")
-    @ResponseBody
     public Object authorize(HttpServletRequest request) throws OAuthProblemException, OAuthSystemException {
-        System.out.printf("===========");
         //构建OAuth授权请求
         OAuthAuthzRequest oAuthRequest=new OAuthAuthzRequest(request);
         //检查传入的客户端id是否正确
---
+++ src/test/java/TestShiro.java
@@ -0,0 +1,42 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+/**
+ * Created by panweiji on 2017/1/13 0013.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)        //表示继承了SpringJUnit4ClassRunner类
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+public class TestShiro {
+    @Test
+    public void testShiroLogin() {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+        }
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+        //6、退出
+        subject.logout();
+    }
+}
